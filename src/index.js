e418fb8baaa4d491f2561d36bb5bd07cb84e160c
kangze <kangze25@hotmail.com>
2018-07-13 18:04:00 +0800

    1.add chrome debugger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/header";
-import { hot } from 'react-hot-loader';
+// import { hot } from 'react-hot-loader';
 
 // let App=hot(module)(Header);
 
