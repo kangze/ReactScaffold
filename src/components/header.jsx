69d8fb407040de27f0d9fdad1b0bcaf7fe9e1532
kangze <kangze25@hotmail.com>
2018-08-30 18:20:53 +0800

    1.开始尝试加入office ui fabric

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,14 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
-require("./header.css")
-export default class Header extends Component{
-    render(){
+//require("./header.css")
+export default class Header extends Component {
+    render() {
         return (
-            <div>hot Module Test1234</div>
+            <Fabric>
+                <DefaultButton>I am a button.</DefaultButton>
+            </Fabric>
         );
     }
 }
