54c944a9316591d8269041f0f81e0d1d3092d519
kangze <kangze25@hotmail.com>
2018-09-07 16:59:32 +0800

    1.add edit box

--- src/components/_shared/header.jsx
+++ src/components/_shared/header.jsx
@@ -5,63 +5,81 @@ import { CommandBarButton, IButtonProps } from 'office-ui-fabric-react/lib/Butto
 
 
 require("./header.css");
+
+let childrenMenus = {
+    items: [
+        {
+            key: 'profile',
+            text: 'Profile',
+            iconProps: { iconName: 'Encryption' },
+            href: "http://bing.com"
+        },
+        {
+            key: 'logout',
+            text: 'Logout',
+            iconProps: { iconName: 'DRM' },
+            onClick: function () { alert("logout") }
+        }
+    ]
+};
+
 export default class Header extends Component {
 
     open() {
         console.log("this");
     }
 
-    render() {
-        let childrenMenus = {
-            items: [
-                {
-                    key: 'profile',
-                    text: 'Profile',
-                    iconProps: { iconName: 'Encryption' },
-                    href: "http://bing.com"
-                },
-                {
-                    key: 'logout',
-                    text: 'Logout',
-                    iconProps: { iconName: 'DRM' },
-                    onClick: function () { alert("logout") }
-                }
-            ]
-        };
 
+
+
+    render() {
         return (
             <div className="ms-Grid-row">
                 <div className="ms-Grid-col ms-sm12 ms-hiddenMdUp">
-                    <div className="header">
-                        <div className="header-1">
-                            <i className="ms-Icon ms-Icon--Bold" aria-hidden="true" />
-                            <span>Money Memory</span>
-                        </div>
-                    </div>
+                    {this.render_mobile()}
                 </div>
                 <div className="ms-Grid-col ms-hiddenSm ms-md12">
-                    <div className="header">
-                        <div className="header-pc-1">
-                            <i className="ms-Icon ms-Icon--Bold" aria-hidden="true" />
-                            <span>Money Memory  </span><small>--Log the life with Money</small>
-                        </div>
-                        <div className="header-pc-2">
-                            <a href="#">Login</a>
-                            &nbsp;&nbsp;&nbsp;
+                    {this.render_pc()}
+                </div>
+            </div>
+        );
+    }
+
+    render_mobile() {
+
+        return (
+            <div className="header">
+                <div className="header-1">
+                    <i className="ms-Icon ms-Icon--Bold" aria-hidden="true" />
+                    <span>Money Memory</span>
+                </div>
+            </div>
+        );
+    }
+
+    render_pc() {
+        
+        return (
+            <div className="header">
+                <div className="header-pc-1">
+                    <i className="ms-Icon ms-Icon--Bold" aria-hidden="true" />
+                    <span>Money Memory  </span><small>--Log the life with Money</small>
+                </div>
+                <div className="header-pc-2">
+                    <a href="#">Login</a>
+                    &nbsp;&nbsp;&nbsp;
                             <a href="#">Logout</a>
-                            &nbsp;&nbsp;&nbsp;
-                            <div style={{ display: "inline-flex" }}>
-                                <CommandBarButton
-                                    style={{ backgroundColor: "#7d8793", fontSize: "15px" }}
-                                    data-automation-id="test"
-                                    onClick={this.open.bind(this)}
-                                    text="kangze25@hotmail.com"
-                                    menuProps={childrenMenus}
-                                />
-                            </div>
-                            <i className="ms-Icon ms-Icon--Contact" aria-hidden="true" />
-                        </div>
-                    </div>
+                    &nbsp;&nbsp;&nbsp;
+
+                        <CommandBarButton
+                        style={{ backgroundColor: "#7d8793" }}
+                        className="account"
+                        data-automation-id="test"
+                        onClick={this.open.bind(this)}
+                        text="kangze25@hotmail1.com"
+                        menuProps={childrenMenus}
+                    />
+                    <i className="ms-Icon ms-Icon--Contact" aria-hidden="true" />
                 </div>
             </div>
         );
--- src/components/publishBox/PublishBox.jsx
+++ src/components/publishBox/PublishBox.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from "react";
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
 //import component
 import { Label } from 'office-ui-fabric-react/lib/Label';
+import { DefaultButton, IconButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
+
 
 export default class PublishBox extends Component {
+
+    handleClick() {
+        alert("clicked me!");
+    }
+
     render() {
         return (
             <div className="ms-Grid-row">
-                <div className="ms-Grid-col ms-hiddenSm ms-md12">
+                <div className="ms-Grid-col ms-md2">
+                    <Label>I'm a Label</Label>
+                </div>
+                <div className="ms-Grid-col ms-md8">
+                    <TextField label="To Publish My State!!" multiline autoAdjustHeight />
+                    <div style={{"backgroundColor":"red","float":"right"}}>
+                    <DefaultButton
+                        primary
+                        data-automation-id="test"
+                        text="Create account"
+                        onClick={this.handleClick.bind(this)}
+                        split={true}
+                        aria-roledescription={'split button'}
+                        style={{
+                            height: '35px', float: "right"
+                        }}
+                        menuProps={{
+                            items: [
+                                {
+                                    key: 'emailMessage',
+                                    text: 'Email message',
+                                    iconProps: { iconName: 'Mail' }
+                                },
+                                {
+                                    key: 'calendarEvent',
+                                    text: 'Calendar event',
+                                    iconProps: { iconName: 'Calendar' }
+                                }
+                            ]
+                        }}
+                    />
+                    </div>
+                </div>
+                <div className="ms-Grid-col ms-md2">
                     <Label>I'm a Label</Label>
                 </div>
             </div>
